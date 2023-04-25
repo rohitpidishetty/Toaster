6efbca542d6bd3f719815b93f0d54e45f9dccfd6
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2023-04-25 12:26:34 +0530

    Update Toast.js

--- Toast.js
+++ Toast.js
@@ -1,4 +1,4 @@
-export function createToast(parent, text) {
+export function createToast(parent, text, status) {
   document.body.innerHTML += `<style>
   ._i2__Lk {
     animation: __inX_4 3s ease-out;
@@ -52,33 +52,82 @@ export function createToast(parent, text) {
       width: 80%;
     }
   }
+  ._p_gbd3__{
+    -webkit-animation-play-state:paused;
+    -moz-animation-play-state:paused;
+    -o-animation-play-state:paused; 
+    animation-play-state:paused;
+  }
 </style>
 <div
   class="_X42LaS__sd x_23d__hd"
   style="
     margin: 10px;
     border-radius: 5px;
-    background-color: black;
+    background-color: white;
     opacity: 0.75;
-    color: white;
     position: fixed;
+    box-shadow: 0px 0px 8px 2px rgb(241, 234, 234);
     z-index: 2;
+    color: rgb(82, 55, 55);
     top: 0;
-    padding-top: 10px;
+    cursor: pointer;
+    padding-top: 3px;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, Roboto,
       Oxygen, Ubuntu, Cantarell, sans-serif;">
   <img
     width="11px"
     style="cursor: pointer"
     src="https://rohitpidishetty.github.io/Toaster/cancel.png"/>
-  <center id="_kh43__hsg">Display your text</center>
+  <span style="display:flex; align-items:center; padding-left:20px">
+    <img src="https://rohitpidishetty.github.io/Toaster/cancel.png" style="margin-right:15px" width="20px" class="__sx_23js" />
+    <center id="_kh43__hsg">Display your text</center>
+  </span>
   <p
     id="_fjhgGK_Ld"
     style="
-      border: 2px solid rgb(5, 206, 5);
       margin-bottom: 0;
       border-radius: 0px 0px 50px 50px;"></p>
 </div>`;
+  switch (status) {
+    case "ok":
+      document
+        .querySelector(".__sx_23js")
+        .setAttribute(
+          "src",
+          "https://rohitpidishetty.github.io/Toaster/accept.png"
+        );
+      document.querySelector("#_fjhgGK_Ld").style.border = "2px solid #4BAE4F";
+      break;
+    case "info":
+      document
+        .querySelector(".__sx_23js")
+        .setAttribute(
+          "src",
+          "https://rohitpidishetty.github.io/Toaster/info.png"
+        );
+      document.querySelector("#_fjhgGK_Ld").style.border = "2px solid #00CCFF";
+      break;
+    case "err":
+      document
+        .querySelector(".__sx_23js")
+        .setAttribute(
+          "src",
+          "https://rohitpidishetty.github.io/Toaster/disagree.png"
+        );
+      document.querySelector("#_fjhgGK_Ld").style.border = "2px solid #FF2147";
+      break;
+    case "warn":
+      document
+        .querySelector(".__sx_23js")
+        .setAttribute(
+          "src",
+          "https://rohitpidishetty.github.io/Toaster/warning.png"
+        );
+      document.querySelector("#_fjhgGK_Ld").style.border = "2px solid #FFD54F";
+      break;
+  }
+
   let width = document.querySelector("._X42LaS__sd").clientWidth - 4 + "px";
   document.getElementById("_fjhgGK_Ld").style.width = width;
   document.querySelector("._X42LaS__sd > img").style.marginLeft =
@@ -89,12 +138,20 @@ export function createToast(parent, text) {
       document.getElementById("_fjhgGK_Ld").classList.toggle("_i2__Lk");
       document.querySelector("._X42LaS__sd").classList.add("x_23d__hd");
       document.querySelector("._X42LaS__sd").classList.toggle("_gU_L23");
-       location.reload();
-    }, 3000);
+      location.reload();
+    }, 2000);
+  });
+  document.querySelector("._X42LaS__sd").addEventListener("mouseover", e => {
+    e.target.classList.add("_p_gbd3__");
+  });
+  document.querySelector("._X42LaS__sd").addEventListener("mouseout", e => {
+    e.target.classList.remove("_p_gbd3__");
   });
   parent.disabled = true;
   document.querySelector("._X42LaS__sd").classList.remove("x_23d__hd");
-  document.querySelector("._X42LaS__sd > #_kh43__hsg").innerHTML = text;
+  document.querySelector(
+    "._X42LaS__sd  > span >  #_kh43__hsg"
+  ).innerHTML = text;
   document.getElementById("_fjhgGK_Ld").classList.toggle("_i2__Lk");
   document.querySelector("._X42LaS__sd").classList.toggle("_gU_L23");
   setTimeout(() => {
